76cd4c3e39a29ba11eade2041382d55aa36e97c9
newhorizon-tech <alaukikinbox@gmail.com>
2021-01-12 01:37:10 +0530

    Fix models

--- db/migrate/20210111200639_create_votes.rb
+++ db/migrate/20210111200639_create_votes.rb
@@ -1,8 +1,8 @@
 class CreateVotes < ActiveRecord::Migration[6.1]
   def change
     create_table :votes do |t|
-      t.user :references
-      t.article :references
+      t.references :user, null: false, foreign_key: true
+      t.references :article, null: false, foreign_key: true
 
       t.timestamps
     end
