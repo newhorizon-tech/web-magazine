8d29823d9bfa38bd796050f9cbc695de5670c3fd
newhorizon-tech <alaukikinbox@gmail.com>
2021-01-13 03:07:54 +0530

    Add one to many relation for Article/Category models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
 class ArticlesController < ApplicationController
+  def new
+  end
+  def create
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  has_many :articles, foreign_key: 'author_id'
+  has_many :votes
 end
