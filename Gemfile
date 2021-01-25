8cfa4c322abbe004072e96dd50958d5e79743141
newhorizon-tech <alaukikinbox@gmail.com>
2021-01-26 02:42:35 +0530

    Add RSpec tests

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.1.0'
@@ -34,6 +33,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,25 @@
+# spec/models/article_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe Article, :type => :model do
+
+  let!(:test_user){User.create(name: 'Test')}
+  
+  it "is not valid without an author" do
+    article = Article.new(author: nil, title: nil,)
+    expect(article).to_not be_valid
+  end
+
+  it "is not valid without a title" do
+    article = Article.new(author: test_user, title: nil,)
+    expect(article).to_not be_valid
+  end
+  it "is not valid with a short title" do
+    article = Article.new(author: test_user,title: '123')
+    article.save
+    puts article.errors.full_messages.inspect
+    expect(article).to_not be_valid
+  end
+  # it "is valid with valid attributes"
+end
