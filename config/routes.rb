50edc684b359cc53c9474c4da9c30eabd5c5f085
newhorizon-tech <alaukikinbox@gmail.com>
2021-01-31 00:12:27 +0530

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root 'categories#index'
-  
-  get 'random', to: 'articles#random', as: :random
+
   get 'logout', to: 'users#destroy', as: :logout
+  resources :users, only: [:new, :create]
 
-  resources :users, :articles, :categories
-
-  resources :articles do
+  resources :articles, only: [:new, :show] do
     post 'upvote', on: :member
+    get 'random', to: 'articles#random', on: :collection
   end
+
+  resources :categories, only: [:index, :show]
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
